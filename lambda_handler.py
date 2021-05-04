fc2465b34d345e724afef266391b98747234c30e
tejaswini3006 <67182120+tejaswini3006@users.noreply.github.com>
2021-05-04 14:40:25 +0530

    commit indesx file

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,7 +3,4 @@ import json
 def lambda_handler(event, context):
     # TODO implement
 	print("these are the changes that need tpo be deployed")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    #return {}
